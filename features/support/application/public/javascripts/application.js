28cbb5a937c6c51beb32ce1ec0a48cc6dc45a82d
Tristan Dunn <tristanzdunn@gmail.com>
2011-12-07 01:29:11 -0600

    Use the DOMContentLoaded event instead of the load event.

--- features/support/application/public/javascripts/application.js
+++ features/support/application/public/javascripts/application.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+window.addEventListener("DOMContentLoaded", function() {
   // Create the client instance.
   Pusher.instance = new Pusher("API_KEY");
 
