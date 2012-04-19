0d40d6436faa2f8d41fa29bc5ace896c2aee9cb4
Thomas Walpole <twalpole@gmail.com>
2012-04-19 13:39:14 -0700

    make subscription_data match pusher 1.11 format

--- features/step_definitions/channel_steps.rb
+++ features/step_definitions/channel_steps.rb
@@ -26,6 +26,7 @@ When %{I subscribe to the "$channel" channel with presence events} do |channel|
         var
         element = list.appendChild(document.createElement("li"));
         element.setAttribute("id", "client-" + client.id);
+        element.innerHTML = client.info && client.info.name
       });
     });
     channel.bind("pusher:member_added", function(client) {
@@ -34,6 +35,7 @@ When %{I subscribe to the "$channel" channel with presence events} do |channel|
       var
       element = list.appendChild(document.createElement("li"));
       element.setAttribute("id", "client-" + client.id);
+      element.innerHTML = client.info && client.info.name
     });
     channel.bind("pusher:member_removed", function(client) {
       var item = list.querySelector("li#client-" + client.id);
