88e23c48aa15f872cbc5236d80ee01effcc580ac
Tristan Dunn <tristanzdunn@gmail.com>
2021-08-10 21:43:14 -0400

    Update dependencies.
    * Updates rake, rubocop, rubocop-performance, rubocop-rake, and rubocop-rspec
      for Ruby.
    * Replaces poltergeist with selenium-webdriver.
    * Replaces webrick with puma.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
-require "capybara/poltergeist"
 require "capybara/rspec"
 
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, url_whitelist: [])
-end
-
 Capybara.app = Sinatra::Application
-Capybara.server = :webrick
-Capybara.default_driver = :poltergeist
+Capybara.server = :puma, { Silent: true }
+Capybara.default_driver = :selenium_chrome_headless
--- spec/support/pusher_fake.rb
+++ spec/support/pusher_fake.rb
@@ -7,7 +7,7 @@ RSpec.configure do |config|
     PusherFake.configuration.reset!
     PusherFake.configuration.web_options.tap do |options|
       Pusher.url = "http://PUSHER_API_KEY:PUSHER_API_SECRET@" \
-                  "#{options[:host]}:#{options[:port]}/apps/PUSHER_APP_ID"
+                   "#{options[:host]}:#{options[:port]}/apps/PUSHER_APP_ID"
     end
 
     @thread = Thread.new { PusherFake::Server.start }
