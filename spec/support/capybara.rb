367325573b68b74ea9c5c6af23328e375b1b2250
Tristan Dunn <tristanzdunn@gmail.com>
2023-03-25 16:53:09 -0400

    Update development dependencies.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,6 +5,3 @@ require "capybara/rspec"
 Capybara.app = Sinatra::Application
 Capybara.server = :puma, { Silent: true }
 Capybara.default_driver = :selenium_chrome_headless
-
-# Ignore deprecation warning.
-Selenium::WebDriver.logger.ignore(:browser_options)
